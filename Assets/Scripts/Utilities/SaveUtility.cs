30681a6938c0bc3d07c03fa04def9021e071aa9a
Edgars Skrabins <shiftkiro@gmail.com>
2025-01-01 22:32:17 +0200

    Better constant management for player prefs keys

--- Assets/Scripts/Utilities/SaveUtility.cs
+++ Assets/Scripts/Utilities/SaveUtility.cs
@@ -2,11 +2,6 @@ using UnityEngine;
 
 namespace Collectives.Utilities
 {
-    public enum EPlayerPrefs
-    {
-        PLAYER_DATA,
-    }
-
     public struct PlayerData
     {
         public int level;
@@ -21,20 +16,25 @@ namespace Collectives.Utilities
 
     public static class SaveUtility
     {
+        private struct PlayerPrefsKeys
+        {
+            public const string PLAYER_DATA = "PLAYER_DATA";
+        }
+
         public static void SavePlayerData(PlayerData _playerData)
         {
             string dataJson = JsonUtility.ToJson(_playerData);
             dataJson = Base64Utility.GetBase64EncodedString(dataJson);
-            PlayerPrefs.SetString(EPlayerPrefs.PLAYER_DATA.ToString(), dataJson);
+            PlayerPrefs.SetString(PlayerPrefsKeys.PLAYER_DATA, dataJson);
         }
 
         public static PlayerData GetSavedPlayerData()
         {
-            if (!PlayerPrefs.HasKey(EPlayerPrefs.PLAYER_DATA.ToString()))
+            if (!PlayerPrefs.HasKey(PlayerPrefsKeys.PLAYER_DATA))
             {
                 return new PlayerData(0, 0);
             }
-            string dataJson = PlayerPrefs.GetString(EPlayerPrefs.PLAYER_DATA.ToString());
+            string dataJson = PlayerPrefs.GetString(PlayerPrefsKeys.PLAYER_DATA);
             dataJson = Base64Utility.GetBase64DecodedString(dataJson);
             return JsonUtility.FromJson<PlayerData>(dataJson);
         }
