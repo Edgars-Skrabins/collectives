d14c2e24ecdcab39146313a63706991aeef73b19
indcrediBro <chirag.agarwal8393@gmail.com>
2025-01-06 01:41:37 +0530

    Added Player Stamina And Sprinting.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,11 +6,19 @@ namespace Collectives.PlayerSystems
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private CharacterController m_controller;
-        [SerializeField] private float m_moveSpeed;
+        [SerializeField] private PlayerStamina m_staminaSystem;
+        [SerializeField] private PlayerGroundCheck m_groundCheck;
+
+        [SerializeField] private float m_walkSpeed;
+        [SerializeField] private float m_sprintSpeedMultiplier;
+        private float m_moveSpeed;
+
+        private bool m_isSprinting;
 
         private void FixedUpdate()
         {
             HandleMovement(GetMovementDirection());
+            HandleStamina();
         }
 
         private void HandleMovement(Vector3 _moveDirection)
@@ -18,8 +26,24 @@ namespace Collectives.PlayerSystems
             Move(_moveDirection);
         }
 
+        private void HandleStamina()
+        {
+            if (m_isSprinting)
+            {
+                m_staminaSystem.DrainStamina(Time.deltaTime);
+            }
+            else if (m_staminaSystem.GetCurrentStamina() < m_staminaSystem.GetMaxStamina())
+            {
+                bool isStanding = m_groundCheck.IsGrounded() && GetMovementDirection() == Vector2.zero;
+                m_staminaSystem.RegenerateStamina(isStanding, Time.deltaTime);
+            }
+        }
+
         private void Move(Vector2 _input)
         {
+            m_isSprinting = Input.GetKey(KeyCode.LeftShift) && m_staminaSystem.CanSprint() && m_groundCheck.IsGrounded();
+            m_moveSpeed = m_isSprinting ? m_walkSpeed * m_sprintSpeedMultiplier : m_walkSpeed;
+
             Vector3 movementVector = transform.right * _input.x + transform.forward * _input.y;
             m_controller.Move(movementVector * m_moveSpeed * Time.deltaTime);
         }
