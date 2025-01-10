b130ccfc63457588a67d7eefe2d36f691b4d07a7
indcrediBro <chirag.agarwal8393@gmail.com>
2025-01-11 03:18:59 +0530

    Updated To State Machine

---
+++ Assets/Scripts/AI/Cameras/CameraLookBehaviourState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Collectives.AICameraSystems
+{
+    public abstract class CameraLookBehaviourState
+    {
+        protected readonly Transform ObjectToRotate;
+
+        protected CameraLookBehaviourState(Transform objectToRotate)
+        {
+            ObjectToRotate = objectToRotate;
+        }
+
+        public abstract IEnumerator Execute(LookAction action);
+    }
+
+    [System.Serializable]
+    public class LookAction
+    {
+        public CameraLookBehaviourTypes actionType;
+        public Vector3 targetPosition;
+        public float timeDuration;
+    }
+}
---
+++ Assets/Scripts/AI/Cameras/CameraWaitState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Collectives.AICameraSystems
+{
+    public class CameraWaitState : CameraLookBehaviourState
+    {
+        public CameraWaitState(Transform objectToRotate) : base(objectToRotate) { }
+
+        public override IEnumerator Execute(LookAction action)
+        {
+            yield return new WaitForSeconds(action.timeDuration);
+        }
+    }
+}
