dba601279083a0c5948703e56cdd05c5ee6b60fb
Edgars-Skrabins <shiftkiro@gmail.com>
2025-01-16 12:44:28 +0200

    Removed useless try catch

--- Assets/Scripts/UI/GlobalUINavigation.cs
+++ Assets/Scripts/UI/GlobalUINavigation.cs
@@ -1,6 +1,4 @@
-using System;
 using Collectives.Utilities.Constants;
-using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Collectives.UI
@@ -9,15 +7,7 @@ namespace Collectives.UI
     {
         public static void GoToScene(EGameScenes _scene)
         {
-            try
-            {
-                SceneManager.LoadScene((int)_scene);
-            }
-            catch (Exception exception)
-            {
-                Debug.LogError("Failed to load scene: " + _scene);
-                throw;
-            }
+            SceneManager.LoadScene((int)_scene);
         }
     }
 }
