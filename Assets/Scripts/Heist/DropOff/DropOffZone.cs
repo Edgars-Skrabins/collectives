47e7d07b09baf14998c8b9dd92cd6c3c409f6c87
Edgars-Skrabins <shiftkiro@gmail.com>
2025-01-25 22:01:24 +0200

    DropOffZone collisions

--- Assets/Scripts/Heist/DropOff/DropOffZone.cs
+++ Assets/Scripts/Heist/DropOff/DropOffZone.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Collectives
 {
     public class DropOffZone : MonoBehaviour
     {
         public Collider m_collider;
+        public UnityEvent OnItemEnterDropOff;
+
+        public void OnItemCollisionInDropOff(Collider _collider)
+        {
+            // if (_collider.TryGetComponent(IVa))
+        }
     }
 }
